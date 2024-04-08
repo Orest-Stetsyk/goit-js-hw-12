f499abda1b7c50ec340d7b6cd9f4638f0fc35633
Orest-Stetsyk <62755155+Orest-Stetsyk@users.noreply.github.com>
2024-04-08 19:32:25 +0300

    fix

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -7,7 +7,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { elem,hideLoading, lightbox, displayToast, showButton } from "../main";
 
 export async function render(data) {
-  if (data.hits.length === 0) {
+  if (data.totalHits.length === 0) {
     hideLoading(); 
     displayToast("Sorry, there are no images matching your search query. Please try again!");
      
